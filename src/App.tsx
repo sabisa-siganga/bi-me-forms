5540637407e0688c1602f9ca08f1e062daa81f8b
Sabisa Siganga <lumisabisa@gmail.com>
2024-11-13 16:13:01 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
-import { useEffect, useState } from 'react';
-import { AppStyled } from './AppStyled';
-import RenderForms from './components/RenderForms/RenderForms';
-import { QuestionDataInterface } from './interface/question';
-import QuestionList from '../questions.json';
+import { useEffect, useState } from "react";
+import { AppStyled } from "./AppStyled";
+import RenderForms from "./components/RenderForms/RenderForms";
+import { QuestionDataInterface } from "./interface/question";
+import QuestionList from "../questions.json";
 
 function App() {
-	const [loading, setLoading] = useState(true);
-	const [data, setData] = useState<QuestionDataInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState<QuestionDataInterface[]>([]);
 
-	function fakeRequest() {
-		setTimeout(() => {
-			const results =
-				QuestionList.questions as never as QuestionDataInterface[];
+  function fakeRequest() {
+    setTimeout(() => {
+      const results =
+        QuestionList.questions as never as QuestionDataInterface[];
 
-			setData(results);
+      setData(results);
 
-			setLoading(false);
-		}, 2000);
-	}
+      setLoading(false);
+    }, 2000);
+  }
 
-	useEffect(() => {
-		setLoading(true);
-    fakeRequest()
-	}, []);
+  useEffect(() => {
+    setLoading(true);
+    fakeRequest();
+  }, []);
 
-	return (
-		<AppStyled>
-			<h1>Bi-me Forms</h1>
-			{!loading && <RenderForms questions={data} />}
+  return (
+    <AppStyled>
+      <h1>Forms</h1>
+      {!loading && <RenderForms questions={data} />}
 
-			{loading && <h2>Fetching data...</h2>}
-		</AppStyled>
-	);
+      {loading && <h2>Fetching data...</h2>}
+    </AppStyled>
+  );
 }
 
 export default App;
